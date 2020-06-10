56898e2bc982743f09c8202f6bf873e77434bcfa
Alessandro <alex.ixs@hotmail.com>
2020-06-10 10:45:28 +0200

    Cambio img con il click sul cerchietto (nav)

--- main.js
+++ main.js
@@ -16,6 +16,15 @@ $(document).ready(
       }
     );
 
+
+    // Click sul Cerchietto
+    $('.nav i').click(
+      function() {
+        var indexCircle = $(this).index();
+        clickOnCircle(indexCircle);
+      }
+    );
+
   }
 );
 
@@ -26,9 +35,14 @@ $(document).ready(
 function nextImage() {
   // Variabili da gestire
   var activeImage = $('.images img.active');
+  var firstImage = $('.images .first');
+  // Gestione navigazione ( Cerchietti )
   var activeCircle = $('.nav i.active');
+  var firstCircle = $('.nav i.first');
 
-  var nextActiveImage = activeImage.next()
+  // Variabili per attivare le immagini SUCCESSIVE con ( .next() )
+  var nextActiveImage = activeImage.next('img');
+  var nextActiveCircle = activeCircle.next('i');
 
   // Togliamo la classe active dalle variabili correnti
   activeImage.fadeOut().removeClass('active').hide();
@@ -37,11 +51,11 @@ function nextImage() {
   // Aggiungiamo la classe active all'img e cerchio sucessivo a rotazione
   // andando a destra
   if(activeImage.hasClass('last')) {
-    $('.images .first').fadeIn().addClass('active');
-    $('.nav i.first').addClass('active');
+    firstImage.fadeIn().addClass('active');
+    firstCircle.addClass('active');
   } else {
       nextActiveImage.fadeIn().addClass('active');
-      activeCircle.next('i').addClass('active');
+      nextActiveCircle.addClass('active');
     }
 }
 
@@ -49,9 +63,14 @@ function nextImage() {
 function prevImage() {
   // Variabili da gestire
   var activeImage = $('.images img.active');
+  var lastImage = $('.images img.last');
+  // Gestione navigazione ( Cerchietti )
   var activeCircle = $('.nav i.active');
+  var lastCircle = $('.nav i.last');
 
+  // Variabili per attivare le immagini PRECEDENTI con ( .prev() )
   var prevActiveImage = activeImage.prev('img');
+  var prevActiveCircle = activeCircle.prev('i');
 
   // Aggiungiamo la classe active all'img e cerchio precedente a rotazione
   // andando a sinistra
@@ -60,10 +79,20 @@ function prevImage() {
 
   // Togliamo la classe active
   if(activeImage.hasClass('first')) {
-    $('.images img.last').fadeIn().addClass('active');
-    $('.nav i.last').addClass('active');
+    lastImage.fadeIn().addClass('active');
+    lastCircle.addClass('active');
   } else {
       prevActiveImage.fadeIn().addClass('active');
-      activeCircle.prev('i').addClass('active');
+      prevActiveCircle.addClass('active');
     }
+}
+
+
+// Function click on lastCircle
+function clickOnCircle(index) {
+  $('.images img.active').fadeOut().removeClass('active').hide();
+  $('.nav i.active').removeClass('active');
+
+  $('.images img').eq(index).fadeIn().addClass('active').show();
+  $('.nav i').eq(index).addClass('active');
 }
